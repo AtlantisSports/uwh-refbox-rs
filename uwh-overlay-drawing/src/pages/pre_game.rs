f11807230b79e1c16a7a05736975d2975cbbee54
Arun Parolikkal <actuday6418@gmail.com>
2022-09-20 14:39:23 +0530

    Fix pre game display

--- uwh-overlay-drawing/src/pages/pre_game.rs
+++ uwh-overlay-drawing/src/pages/pre_game.rs
@@ -13,40 +13,42 @@ impl PageRenderer {
                 draw_texture(self.textures.bottom_graphic, 0_f32, 0f32, WHITE);
                 let min = state.snapshot.secs_in_period / 60;
                 let secs = state.snapshot.secs_in_period % 60;
-                let text = format!(
-                    "{}:{}",
-                    if min < 10 {
-                        format!("0{}", min)
-                    } else {
-                        format!("{}", min)
-                    },
-                    if secs < 10 {
-                        format!("0{}", secs)
-                    } else {
-                        format!("{}", secs)
-                    }
-                );
-                let x_off = center_text_offset!(90f32, text.as_str(), 50, self.textures.font);
-                draw_text_ex(
-                    text.as_str(),
-                    870f32 + x_off,
-                    1020f32,
-                    TextParams {
-                        font: self.textures.font,
-                        font_size: 50,
-                        ..Default::default()
-                    },
-                );
-                draw_text_ex(
-                    "NEXT GAME",
-                    905f32,
-                    1044f32,
-                    TextParams {
-                        font: self.textures.font,
-                        font_size: 20,
-                        ..Default::default()
-                    },
-                );
+                if !self.is_alpha_mode {
+                    let text = format!(
+                        "{}:{}",
+                        if min < 10 {
+                            format!("0{}", min)
+                        } else {
+                            format!("{}", min)
+                        },
+                        if secs < 10 {
+                            format!("0{}", secs)
+                        } else {
+                            format!("{}", secs)
+                        }
+                    );
+                    let x_off = center_text_offset!(90f32, text.as_str(), 50, self.textures.font);
+                    draw_text_ex(
+                        text.as_str(),
+                        870f32 + x_off,
+                        1020f32,
+                        TextParams {
+                            font: self.textures.font,
+                            font_size: 50,
+                            ..Default::default()
+                        },
+                    );
+                    draw_text_ex(
+                        "NEXT GAME",
+                        905f32,
+                        1044f32,
+                        TextParams {
+                            font: self.textures.font,
+                            font_size: 20,
+                            ..Default::default()
+                        },
+                    );
+                }
             }
             15 => {
                 // animate a fade on the fifteenth second in the alpha stream
@@ -56,64 +58,66 @@ impl PageRenderer {
                 draw_texture(
                     self.textures.atlantis_logo_graphic,
                     0_f32,
-                    0f32,
+                    0_f32,
                     Color::from_rgba(255, 255, 255, if self.is_alpha_mode { offset } else { 255 }),
                 );
                 draw_texture(
                     self.textures.bottom_graphic,
                     0_f32,
-                    0f32,
+                    0_f32,
                     Color::from_rgba(255, 255, 255, if self.is_alpha_mode { offset } else { 255 }),
                 );
-                let min = state.snapshot.secs_in_period / 60;
-                let secs = state.snapshot.secs_in_period % 60;
-                let text = format!(
-                    "{}:{}",
-                    if min < 10 {
-                        format!("0{}", min)
-                    } else {
-                        format!("{}", min)
-                    },
-                    if secs < 10 {
-                        format!("0{}", secs)
-                    } else {
-                        format!("{}", secs)
-                    }
-                );
-                let x_off = center_text_offset!(90f32, text.as_str(), 50, self.textures.font);
-                draw_text_ex(
-                    text.as_str(),
-                    870f32 + x_off,
-                    1020f32,
-                    TextParams {
-                        font: self.textures.font,
-                        font_size: 50,
-                        color: Color::from_rgba(
-                            255,
-                            255,
-                            255,
-                            if self.is_alpha_mode { offset } else { 255 },
-                        ),
-                        ..Default::default()
-                    },
-                );
-                draw_text_ex(
-                    "NEXT GAME",
-                    905f32,
-                    1044f32,
-                    TextParams {
-                        font: self.textures.font,
-                        font_size: 20,
-                        color: Color::from_rgba(
-                            255,
-                            255,
-                            255,
-                            if self.is_alpha_mode { offset } else { 255 },
-                        ),
+                if !self.is_alpha_mode {
+                    let min = state.snapshot.secs_in_period / 60;
+                    let secs = state.snapshot.secs_in_period % 60;
+                    let text = format!(
+                        "{}:{}",
+                        if min < 10 {
+                            format!("0{}", min)
+                        } else {
+                            format!("{}", min)
+                        },
+                        if secs < 10 {
+                            format!("0{}", secs)
+                        } else {
+                            format!("{}", secs)
+                        }
+                    );
+                    let x_off = center_text_offset!(90f32, text.as_str(), 50, self.textures.font);
+                    draw_text_ex(
+                        text.as_str(),
+                        870f32 + x_off,
+                        1020f32,
+                        TextParams {
+                            font: self.textures.font,
+                            font_size: 50,
+                            color: Color::from_rgba(
+                                255,
+                                255,
+                                255,
+                                if self.is_alpha_mode { offset } else { 255 },
+                            ),
+                            ..Default::default()
+                        },
+                    );
+                    draw_text_ex(
+                        "NEXT GAME",
+                        905f32,
+                        1044f32,
+                        TextParams {
+                            font: self.textures.font,
+                            font_size: 20,
+                            color: Color::from_rgba(
+                                255,
+                                255,
+                                255,
+                                if self.is_alpha_mode { offset } else { 255 },
+                            ),
 
-                        ..Default::default()
-                    },
-                );
+                            ..Default::default()
+                        },
+                    );
+                }
             }
             _ => {
                 self.animation_counter = 0f32;
@@ -143,7 +147,7 @@ impl PageRenderer {
             TextParams {
                 font: self.textures.font,
                 font_size: 30,
-                color: BLACK,
+                color: if self.is_alpha_mode { WHITE } else { BLACK },
                 ..Default::default()
             },
         );
@@ -159,7 +163,7 @@ impl PageRenderer {
             TextParams {
                 font: self.textures.font,
                 font_size: 20,
-                color: BLACK,
+                color: if self.is_alpha_mode { WHITE } else { BLACK },
                 ..Default::default()
             },
         );
@@ -177,49 +181,58 @@ impl PageRenderer {
                 ..Default::default()
             },
         );
-        if let Some(flag) = state.white_flag {
-            draw_texture_ex(
-                flag,
-                79f32,
-                39f32,
-                WHITE,
-                DrawTextureParams {
-                    dest_size: Some(vec2(70f32, 33f32)),
+        if !self.is_alpha_mode {
+            draw_text_ex(
+                "15:00",
+                460f32,
+                67f32,
+                TextParams {
+                    font: self.textures.font,
+                    font_size: 50,
                     ..Default::default()
                 },
             );
-        }
-        if let Some(flag) = state.black_flag {
-            draw_texture_ex(
-                flag,
-                79f32,
-                75f32,
-                WHITE,
-                DrawTextureParams {
-                    dest_size: Some(vec2(70f32, 33f32)),
+            draw_text_ex(
+                "1ST HALF",
+                478f32,
+                100f32,
+                TextParams {
+                    font: self.textures.font,
+                    font_size: 20,
                     ..Default::default()
                 },
             );
+            if let Some(flag) = state.white_flag {
+                draw_texture_ex(
+                    flag,
+                    79f32,
+                    39f32,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(70f32, 33f32)),
+                        ..Default::default()
+                    },
+                );
+            }
+            if let Some(flag) = state.black_flag {
+                draw_texture_ex(
+                    flag,
+                    79f32,
+                    75f32,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(70f32, 33f32)),
+                        ..Default::default()
+                    },
+                );
+            }
+        } else {
+            if state.white_flag.is_some() {
+                draw_rectangle(79f32, 39f32, 70f32, 33f32, WHITE);
+            }
+            if state.black_flag.is_some() {
+                draw_rectangle(79f32, 75f32, 70f32, 33f32, WHITE);
+            }
         }
-        draw_text_ex(
-            "15:00",
-            460f32,
-            67f32,
-            TextParams {
-                font: self.textures.font,
-                font_size: 50,
-                ..Default::default()
-            },
-        );
-        draw_text_ex(
-            "1ST HALF",
-            478f32,
-            100f32,
-            TextParams {
-                font: self.textures.font,
-                font_size: 20,
-                ..Default::default()
-            },
-        );
     }
 }

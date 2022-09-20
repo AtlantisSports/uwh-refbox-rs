f26890e1e2a65d23bc0f26897c03ae5038da5f21
Arun Parolikkal <actuday6418@gmail.com>
2022-09-20 14:15:55 +0530

    Show team flags in final scores display

--- uwh-overlay-drawing/src/pages/final_scores.rs
+++ uwh-overlay-drawing/src/pages/final_scores.rs
@@ -9,9 +9,31 @@ impl PageRenderer {
         draw_texture(self.textures.atlantis_logo_graphic, 0_f32, 0f32, WHITE);
         draw_texture(self.textures.final_score_graphic, 0_f32, 0f32, WHITE);
         draw_texture(self.textures.team_information_graphic, 0_f32, 0f32, WHITE);
+        let x_off = center_text_offset!(
+            200f32,
+            state.black.team_name.to_uppercase().as_str(),
+            45,
+            self.textures.font
+        );
+        draw_text_ex(
+            state.black.team_name.to_uppercase().as_str(),
+            1350f32 + x_off,
+            805f32,
+            TextParams {
+                font: self.textures.font,
+                font_size: 45,
+                ..Default::default()
+            },
+        );
+        let x_off = center_text_offset!(
+            215f32,
+            state.white.team_name.to_uppercase().as_str(),
+            45,
+            self.textures.font
+        );
         draw_text_ex(
             state.white.team_name.to_uppercase().as_str(),
-            340f32,
+            135f32 + x_off,
             805f32,
             TextParams {
                 font: self.textures.font,
@@ -20,17 +42,38 @@ impl PageRenderer {
                 ..Default::default()
             },
         );
-        draw_text_ex(
-            state.black.team_name.to_uppercase().as_str(),
-            1240f32,
-            805f32,
-            TextParams {
-                font: self.textures.font,
-                font_size: 45,
-                ..Default::default()
-            },
-        );
-        if !self.is_alpha_mode {
+        if self.is_alpha_mode {
+            if state.white_flag.is_some() {
+                draw_rectangle(580f32, 738f32, 180f32, 100f32, WHITE);
+            }
+            if state.black_flag.is_some() {
+                draw_rectangle(1163f32, 738f32, 180f32, 100f32, WHITE);
+            }
+        } else {
+            if let Some(flag) = state.white_flag {
+                draw_texture_ex(
+                    flag,
+                    580f32,
+                    738f32,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(flag.width() / (flag.height() / 100f32), 100f32)),
+                        ..Default::default()
+                    },
+                );
+            }
+            if let Some(flag) = state.black_flag {
+                draw_texture_ex(
+                    flag,
+                    1163f32,
+                    738f32,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(180f32, 100f32)),
+                        ..Default::default()
+                    },
+                );
+            }
             let x_off = center_text_offset!(
                 135f32,
                 format!("GAME ID: {}", &state.game_id.to_string()).as_str(),

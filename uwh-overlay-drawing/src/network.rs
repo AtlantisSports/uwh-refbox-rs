0889b7b8cb9c2c803765f807e1aff50e5c3f1719
Arun Parolikkal <actuday6418@gmail.com>
2022-09-25 21:32:37 +0530

    show placeholder data when UWH API cannot find the refbox's game

--- uwh-overlay-drawing/src/network.rs
+++ uwh-overlay-drawing/src/network.rs
@@ -43,7 +43,10 @@ impl TeamInfo {
         }
 
         Self {
-            team_name: data["team"]["name"].as_str().unwrap().to_string(),
+            team_name: data["team"]["name"]
+                .as_str()
+                .unwrap_or("Game not found")
+                .to_string(),
             players: player_list,
             flag: data["team"]["flag_url"]
                 .as_str()
@@ -62,15 +65,28 @@ pub struct StatePacket {
     pub start_time: Option<String>,
 }
 
-fn fetch_data(tr: std::sync::mpsc::Sender<Value>, url: String, tournament_id: u32, game_id: u32) {
+fn fetch_data(
+    tr: std::sync::mpsc::Sender<(Value, TeamInfo, TeamInfo)>,
+    url: String,
+    tournament_id: u32,
+    game_id: u32,
+) {
     info!("Requesting game data from UWH API");
     let data = reqwest::blocking::get(format!(
         "https://{}/api/v1/tournaments/{}/games/{}",
         url, tournament_id, game_id
     ))
     .unwrap();
-    let t = data.text().unwrap();
-    tr.send(serde_json::from_str(t.as_str()).unwrap()).unwrap();
+    let text = data.text().unwrap();
+    let data: Value = serde_json::from_str(text.as_str()).unwrap();
+    let team_id_black = Some(data["game"]["black_id"].as_u64().unwrap_or(0));
+    let team_id_white = Some(data["game"]["white_id"].as_u64().unwrap_or(0));
+    tr.send((
+        data,
+        TeamInfo::new(&url, tournament_id, team_id_black.unwrap()),
+        TeamInfo::new(&url, tournament_id, team_id_white.unwrap()),
+    ))
+    .unwrap();
 }
 
 #[tokio::main]
@@ -80,10 +96,8 @@ pub async fn networking_thread(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect((config.refbox_ip, config.refbox_port as u16))
         .expect("Is the refbox running? We error'd out on the connection");
-    let (tr, rc) = std::sync::mpsc::channel::<Value>();
+    let (tr, rc) = std::sync::mpsc::channel::<(Value, TeamInfo, TeamInfo)>();
     let url = config.uwhscores_url.clone();
-    let mut team_id_black;
-    let mut team_id_white;
     let mut buff = vec![0u8; 1024];
     let mut read_bytes;
     let mut game_id = std::u32::MAX;
@@ -104,25 +118,20 @@ pub async fn networking_thread(
                 );
                 std::thread::spawn(move || fetch_data(tr, url, tournament_id, game_id));
             }
-            if let Ok(data) = rc.try_recv() {
-                team_id_black = Some(data["game"]["black_id"].as_u64().unwrap());
-                team_id_white = Some(data["game"]["white_id"].as_u64().unwrap());
+            if let Ok((data, black, white)) = rc.try_recv() {
                 if tx
                     .send(StatePacket {
                         snapshot,
                         game_id: Some(game_id),
-                        black: Some(TeamInfo::new(
-                            &config.uwhscores_url,
-                            tournament_id,
-                            team_id_black.unwrap(),
-                        )),
-                        white: Some(TeamInfo::new(
-                            &config.uwhscores_url,
-                            tournament_id,
-                            team_id_white.unwrap(),
-                        )),
-                        pool: Some(data["game"]["pool"].as_str().unwrap().to_string()),
-                        start_time: Some(data["game"]["start_time"].as_str().unwrap().to_string()),
+                        black: Some(black),
+                        white: Some(white),
+                        pool: Some(data["game"]["pool"].as_str().unwrap_or("").to_string()),
+                        start_time: Some(
+                            data["game"]["start_time"]
+                                .as_str()
+                                .unwrap_or("")
+                                .to_string(),
+                        ),
                     })
                     .is_err()
                 {

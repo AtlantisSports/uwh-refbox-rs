fad2b5d5ac7c133f817654c2b61ed19b3d7a5b5a
Tristan Debrunner <tdebrunner@atlantissports.org>
2022-12-15 18:47:00 -0700

    Bump versions

--- xc/linux-arm/build_containers.sh
+++ xc/linux-arm/build_containers.sh
@@ -1,4 +1,4 @@
-RUST_VERSION="1.63"
+RUST_VERSION="1.66"
 
 docker build -f Dockerfile-aarch64 -t "linux-rust-$RUST_VERSION-xc-aarch64" .
 docker build -f Dockerfile-armv7 -t "linux-rust-$RUST_VERSION-xc-armv7" .

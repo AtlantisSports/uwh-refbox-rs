09e02b820b5fc98ae1227e87f553637faff07a24
Tristan Debrunner <tdebrunner@atlantissports.org>
2023-06-28 12:59:19 -0700

    Remove unused `overtime_break_duration` field from config

--- uwh-common/src/config.rs
+++ uwh-common/src/config.rs
@@ -21,8 +21,6 @@ pub struct Game {
     #[serde(with = "secs_only_duration")]
     pub pre_overtime_break: Duration,
     #[serde(with = "secs_only_duration")]
-    pub overtime_break_duration: Duration,
-    #[serde(with = "secs_only_duration")]
     pub pre_sudden_death_duration: Duration,
     #[serde(with = "secs_only_duration")]
     pub post_game_duration: Duration,
@@ -44,7 +42,6 @@ impl Default for Game {
             ot_half_play_duration: Duration::from_secs(300),
             ot_half_time_duration: Duration::from_secs(180),
             pre_overtime_break: Duration::from_secs(180),
-            overtime_break_duration: Duration::from_secs(60),
             pre_sudden_death_duration: Duration::from_secs(60),
             post_game_duration: Duration::from_secs(120),
             nominal_break: Duration::from_secs(900),

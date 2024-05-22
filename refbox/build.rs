d5a37ba1993e466ead51824abb68100aefcbb4f9
TristanDebrunner <tdebrunner@atlantissports.org>
2024-05-22 13:11:58 -0700

    Ensure that builds support older MacOS versions (#340)

--- refbox/build.rs
+++ refbox/build.rs
@@ -1,9 +1,14 @@
-#[cfg(windows)]
+#[cfg(target_os = "windows")]
 fn main() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("resources/AppIcon.ico");
     res.compile().unwrap();
 }
 
-#[cfg(not(windows))]
+#[cfg(target_os = "macos")]
+fn main() {
+    println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=12");
+}
+
+#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
 fn main() {}

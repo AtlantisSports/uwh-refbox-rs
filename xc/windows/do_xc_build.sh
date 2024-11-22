94a335903969b854a2f6bb33e65a789d7e4a7c2b
TristanDebrunner <tdebrunner@atlantissports.org>
2024-11-21 16:45:21 -0800

    cross compilation updates (#433)
    * Bump rust version numbers
    
    * Don't add target to the Docker context

--- xc/windows/do_xc_build.sh
+++ xc/windows/do_xc_build.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_CMD="cargo build --bin refbox --target x86_64-pc-windows-msvc --release"
 CONTAINER_WORKDIR="/workdir"
 
-CONTAINER_NAME="$(docker create -t -w "$CONTAINER_WORKDIR/" windows-rust-1.77-xc /bin/bash -c "$CONTAINER_CMD")"
+CONTAINER_NAME="$(docker create -t -w "$CONTAINER_WORKDIR/" windows-rust-1.82-xc /bin/bash -c "$CONTAINER_CMD")"
 
 for file in $(ls "$SCRIPT_DIR/../.." | grep -v target | grep -v xc); do
     docker cp "$SCRIPT_DIR/../../$file" "$CONTAINER_NAME:$CONTAINER_WORKDIR/"
